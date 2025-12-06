49d2ee43ce105c47b54a9f6470f40df41a0ca107
mbafousu <carolngue06@gmail.com>
2025-12-06 14:38:52 -0500

    initial commit

---
+++ part1.js
@@ -0,0 +1,37 @@
+// ===========================================
+// Part1: Refactored old code( feeling loopy)
+//using arrays and objets
+//=============================================
+
+// CSV string
+const csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+
+// 1. split into rows by newline
+// 2. First row is the header
+// 3. create and array to store row objects
+
+const rows = csvData.split("\n");
+const headers = rows[0].split(",");
+const records = {};
+
+// 4. Loop through data rows starting at 1
+
+for (let i = 1; i < rows.length; i++) {
+    const cells = rows[i].split(",");
+    const record = {};
+
+
+    
+//5. Match each cell to the correct header
+
+for (let j = 0; j < headers.length; j++) {
+    const key = headers[j];
+    const value = cells[j];
+    record[key] = value;
+
+}
+
+
+
+    console.log(record.ID, record.Name, record.Occupation, record.Age );
+}
